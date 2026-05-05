b7c4d4f19b5da9978837abbea80e151a2b0d5476
comenas <litojaviermadrid@gmail.com>
2026-05-05 08:26:31 +0200

    Terminado el Data_loader
    terminado el data_loader con validaciones y pasar de columna a float

--- Core/data_loader.py
+++ Core/data_loader.py
@@ -1,5 +1,6 @@
 import os 
-import pandas 
+import pandas as pd
+import numpy as np
 
 def validate_file_exists(path):
     """
@@ -23,4 +24,33 @@ def validate_file_extension(path):
         raise ValueError(f"Extensión del archivo {path} erronea") #lanzar error
 
 def read_excel_file(path):
-    return pandas.read_excel(path)
+    """
+    función que lee el archivo excel mediante pandas
+    """
+    return pd.read_excel(path) #el propio pandas lanza el error si algo sale mal
+
+def extract_column_as_float(df, col_idx):
+    """
+    función que transforma cada columna del dataFrame a float
+    """
+    columna = df.iloc[:, col_idx] #extrae columna
+    return columna.to_numpy(dtype = float) #transforma en float
+
+def validate_no_nan(array):
+    """
+    función que valida si existe algún valor Nan dentro del array
+    """
+    validacion = np.isnan(array) #función numpy que devuelve un array de true y false
+    if validacion.any(): #mira el array por si hay algún True 
+        raise ValueError(f"Algún Valor Nan en el array") #si alguno es True lanza error
+    else:
+        return True #si no, pasa
+    
+def validate_min_length(array, min_len):
+    """
+    función que comprueba que en el array hay al menos el mínimo de valores indicados
+    """
+    if len(array) < min_len: #cantidad de valores mayor al mínimo
+        raise ValueError(f"Hay menos valores de los mínimos en el array") #lanza error si no es así
+    else:
+        return True
